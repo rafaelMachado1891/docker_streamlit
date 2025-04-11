a62dc73cac57c90497055cb87f4ba5db0a68f507
rafaelMachado1891 <rafadmn1@gmail.com>
2025-04-11 13:46:55 -0300

    atualização requirements

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import matplotlib
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Dashboard de Vendas",layout="wide")
 
@@ -14,4 +14,22 @@ def gerar_dados():
 
 df = gerar_dados()
 
-print(df)
+st.subheader("Dados de Vendas")
+st.dataframe(df)
+
+st.subheader('Tendencia de Vendas')
+fig, ax = plt.subplots(figsize=(10,4))
+ax.plot(df["mes"], df['vendas'], markers='0')
+ax.set_ylabel('vendas (R$)')
+ax.grid(True)
+st.pyplot(fig)
+
+
+st.subheader("Metricas")
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("Total de Vendas", f"R$ {df["vendas"].sum():, 2f}")
+with col1:
+    st.metric("Média Mensal", f"R$ {df["vendas"].mean():, 2f}")
+with col1:
+    st.metric("Crescimento", f"{((df['vendas'].iloc[-1] / df['vendas'].iloc[0])-1)*100: 1f}%")
