0b929170c78453c1b2065dfe6b401506f2281c8c
rafaelMachado1891 <rafadmn1@gmail.com>
2025-04-14 13:14:09 -0300

    update

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
+
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_title="Dashboard de Vendas",layout="wide")
+st.set_page_config(page_title="Dashboard de Vendas", layout="wide")
 
-st.title("Dashboar de Vendas")
+st.title("Dashboard de Vendas")
 
 def gerar_dados():
-    dados = {"mes": ['jan', 'fev','mar', 'abr', 'mai','jun','jul','ago', 'set', 'out', 'nov', 'dez'],
-     "valor": [5300, 8000, 2000, 1800, 1900, 7000, 10000, 3400, 2500, 1000, 1660, 3000]
-     }
+    dados = {"mes": ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
+             "valor": [5300, 8000, 2000, 1800, 1900, 7000, 10000, 3400, 2500, 1000, 1660, 3000]}
     return pd.DataFrame(dados)
 
 df = gerar_dados()
@@ -17,19 +17,9 @@ df = gerar_dados()
 st.subheader("Dados de Vendas")
 st.dataframe(df)
 
-st.subheader('Tendencia de Vendas')
-fig, ax = plt.subplots(figsize=(10,4))
-ax.plot(df["mes"], df['vendas'], markers='0')
+st.subheader('Tendência de Vendas')
+fig, ax = plt.subplots(figsize=(10, 4))
+ax.plot(df["mes"], df["valor"], marker='o') 
 ax.set_ylabel('vendas (R$)')
 ax.grid(True)
 st.pyplot(fig)
-
-
-st.subheader("Metricas")
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.metric("Total de Vendas", f"R$ {df["vendas"].sum():, 2f}")
-with col1:
-    st.metric("Média Mensal", f"R$ {df["vendas"].mean():, 2f}")
-with col1:
-    st.metric("Crescimento", f"{((df['vendas'].iloc[-1] / df['vendas'].iloc[0])-1)*100: 1f}%")
